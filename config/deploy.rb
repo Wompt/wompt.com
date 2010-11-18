cf619670bded7d32897e246c7db40b84d7652b46
Daniel Beardsley <daniel.beardsley@gmail.com>
2010-11-18 01:22:43 -0800

    Use capistrano for deployment

---
+++ config/deploy.rb
@@ -0,0 +1,25 @@
+load 'deploy' if respond_to?(:namespace) # cap2 differentiator
+
+set :application, "wompt"
+set :user, "ubuntu"
+set :host, "wompt.com"
+
+set :scm, :git
+set :repository, "git@github.com:abtinf/wompt.git"
+set :git_enable_submodules, true
+set :deploy_via, :remote_cache
+
+
+role :app, host
+
+set :use_sudo, true
+set :admin_runner, 'ubuntu'
+set :normalize_asset_timestamps, false
+default_run_options[:pty] = true
+
+task :production do
+  set :deploy_to, "/home/ubuntu/www/#{application}"
+  set :branch, "master"  
+end
+
+load 'config/tasks'
