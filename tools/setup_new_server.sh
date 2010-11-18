7ce5f13a275962d70c91996612bc58216ad9b607
Abtin Forouzandeh <abtinf@gmail.com>
2010-11-18 00:14:41 -0800

    Outline of steps to setup new server.

---
+++ tools/setup_new_server.sh
@@ -0,0 +1,6 @@
+#create instance with create_ec2_instance.sh
+#request ip with ec2-allocate-address
+#associate ip with instance with ec2-associate-address ip_address -i instance_id
+#send server setup script to server
+#execute server setup script
+
