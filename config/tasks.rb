368bdd86e441765cd404e5abba31248afb41b4c8
Daniel Beardsley <daniel.beardsley@gmail.com>
2010-11-19 17:25:48 -0800

    Added Nodejs monit config for wompt and a capistrano task to do the symlinking

--- config/tasks.rb
+++ config/tasks.rb
@@ -12,6 +12,11 @@ namespace :deploy do
     run "#{try_sudo :as => 'root'} restart #{application} || #{try_sudo :as => 'root'} start #{application}"
   end
 
+  desc "Create a symlinks in /etc/monit to the monit files in the deployed app"
+  task "symlink_monit_config" do
+    run "#{try_sudo :as => 'root'} ln -f -s #{current_path}/config/monit/* /etc/monit/"
+  end
+
   desc "save the local version of the upstart script to the server"
   task :send_upstart_script, :roles => :app do
     remote_tempfile = "/tmp/#{application}_upstart.conf"
@@ -47,3 +52,4 @@ UPSTART
 end
 
 after 'deploy:setup', 'deploy:send_upstart_script'
+after 'deploy:cold', 'deploy:symlink_monit_config'
