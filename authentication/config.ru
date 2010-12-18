ede75194bf4342bc5a40158fcb8d5a5555f13021
Daniel Beardsley <daniel.beardsley@gmail.com>
2010-12-18 11:02:25 -0800

    New Authentication via Omniauth utilized as a Sinatra application

---
+++ authentication/config.ru
@@ -0,0 +1,7 @@
+require 'rubygems'
+require 'bundler'
+
+Bundler.require
+
+require './wompt_auth'
+run WomptAuth
---
+++ authentication/wompt_auth.rb
@@ -0,0 +1,26 @@
+require 'sinatra/base'
+require 'openid/store/filesystem'
+
+class OmniAuth::Strategies::OAuth2
+  def full_host
+    uri = URI.parse(request.url.gsub('|','%7C'))
+    uri.path = ''
+    uri.query = nil
+    uri.to_s
+  end
+end
+
+class WomptAuth < Sinatra::Base
+  use OmniAuth::Builder do
+    #provider :facebook , '181725458505189' , '5afa28d747aabd3d1a6ce71d26933c14', :scope => 'email'
+    provider :open_id, OpenID::Store::Filesystem.new('/tmp'), :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id', :scope => 'email'
+  end
+
+  enable :sessions
+  
+  post '/auth/:name/callback' do |name|
+    auth = request.env['omniauth.auth']
+    
+    "#{name} told us that you are legit:<br/> #{auth.inspect}" 
+  end  
+end
