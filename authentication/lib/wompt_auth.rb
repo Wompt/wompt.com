cca8f062a4ac6de6c2aa985514cf0dfa4b213bc4
Daniel Beardsley <daniel.beardsley@gmail.com>
2010-12-20 01:34:39 -0800

    Auth: Support Facebook authentication

--- authentication/lib/wompt_auth.rb
+++ authentication/lib/wompt_auth.rb
@@ -7,15 +7,6 @@ Mongomatic.db = Mongo::Connection.new.db(CONFIG[:database])
 
 ONE_TIME_TOKEN_COOKIE = 'wompt_auth_one_time_token'
 
-class OmniAuth::Strategies::OAuth2
-  def full_host
-    uri = URI.parse(request.url.gsub('|','%7C'))
-    uri.path = ''
-    uri.query = nil
-    uri.to_s
-  end
-end
-
 class WomptAuth < Sinatra::Base
   use Rack::Session::Pool,
     :path => '/auth',
@@ -23,11 +14,16 @@ class WomptAuth < Sinatra::Base
     :secret => 'C6xyESB0FdkabrhtBxOlPikZTS0jKnQRq1vMfluX'
   
   use OmniAuth::Builder do
-    #provider :facebook , '181725458505189' , '5afa28d747aabd3d1a6ce71d26933c14', :scope => 'email'
+    provider :facebook , '181725458505189' , '5afa28d747aabd3d1a6ce71d26933c14', :scope => 'email'
     provider :open_id, nil, :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id', :scope => 'email'
   end
+  
+  def self.get_or_post(path, opts={}, &block)
+    get(path, opts, &block)
+    post(path, opts, &block)
+  end
 
-  post '/auth/:name/callback' do |name|
+  get_or_post '/auth/:name/callback' do |name|
     auth = request.env['omniauth.auth']
     host = request.env['HTTP_HOST'].match(/^([^:]+)(?:\:\d+)$/)[1]
     user = find_or_create_user(auth)
