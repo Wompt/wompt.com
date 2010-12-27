ed1ba31ddd7c1667199dcfa02d997650b96ace8a
Daniel Beardsley <daniel.beardsley@gmail.com>
2010-12-27 02:52:46 -0800

    Removed some comments from server.js

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,17 +1,5 @@
 var wompt     = require("./lib/includes");
 
-/*
- 
-	Objects:
-		App
-		 - has many channels
-		 - has many clients
-
-		Channel
-		 - has many clients
-
-*/
-
 var app = new wompt.App({
 	config: wompt.env,
 	root: __dirname
