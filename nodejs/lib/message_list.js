155119545ad96d57fcb703a3d64e1448b8fbce9f
Daniel Beardsley <daniel.beardsley@gmail.com>
2011-01-27 14:22:09 -0800

    Message List purges upon reaching a certain size complete refactor of Message List, now exposes 'downsize' event

--- nodejs/lib/message_list.js
+++ nodejs/lib/message_list.js
@@ -1,24 +1,41 @@
-exports.MessageList = function(){
+var util = require("util"),
+    events = require("events");
+
+function MessageList(){
 	this.messages = [];
 }
 
-exports.MessageList.prototype = {
-	add: function(msg){
-		this.messages.push(msg);
-	},
-	
-	since: function(time){
-		var msgs = this.messages;
-		
-		var i=msgs.length;
-		while(msgs[--i].time > time) {};
-		
-		return messages.slice(i+1);
-	},
-	
-	recent: function(count){
-		var msgs = this.messages;
-		count = Math.min(msgs.length, count);
-		return msgs.slice(msgs.length-count);
-	}
+util.inherits(MessageList, events.EventEmitter);
+var proto = MessageList.prototype;
+
+proto.add = function(msg){
+	this.messages.push(msg);
+	if(this.messages.length > this.MAX_MESSAGES) this._downsize();
 }
+
+proto._downsize = function(){
+	var new_size = Math.ceil(this.messages.length * this.DOWNSIZE_RATIO);
+	var trimmed_messages = this.messages.splice(0, this.messages.length - new_size);
+	this.emit('downsize', trimmed_messages);
+}
+
+proto.since = function(time){
+	var msgs = this.messages;
+	
+	var i=msgs.length;
+	while(msgs[--i].time > time) {};
+	
+	return messages.slice(i+1);
+}
+	
+proto.recent = function(count){
+	var msgs = this.messages;
+	count = Math.min(msgs.length, count);
+	return msgs.slice(msgs.length-count);
+}
+
+//Constants
+proto.MAX_MESSAGES = 10
+proto.DOWNSIZE_RATIO = 0.5;
+
+exports.MessageList = MessageList;
