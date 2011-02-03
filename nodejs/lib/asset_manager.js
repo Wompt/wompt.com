df9c84a4f5003e028a4964c808723117b7d2d4dd
Daniel Beardsley <daniel.beardsley@gmail.com>
2011-02-03 04:00:08 -0800

    Made asset manager paths use the env.root and public_dir variables

--- nodejs/lib/asset_manager.js
+++ nodejs/lib/asset_manager.js
@@ -1,10 +1,15 @@
+var wompt = require('./includes');
+var root = wompt.env.root;
+var pub = wompt.env.public_dir;
+
+
 var assetManager = require('connect-assetmanager');
 var assetHandler = require('connect-assetmanager-handlers');
 
 var assetManagerGroups = {
 	'landing_js': {
 		'route': /\/js\/landing_[\d]+.js/
-		, 'path': './public/'
+		, 'path': pub
 		, 'dataType': 'javascript'
 		, 'files': [
 			'/external/events.js'
@@ -12,22 +17,22 @@ var assetManagerGroups = {
 	},		
 	'channel_js': {
 		'route': /\/js\/channel_[\d]+.js/
-		, 'path': './'
+		, 'path': root
 		, 'dataType': 'javascript'
 		, 'files': [
-			'public/external/events.js'
-			, 'vendor/Socket.IO/socket.io.js'
-			, 'public/js/client.js'
-			, 'public/external/json2.js'
+			'/public/external/events.js'
+			, '/vendor/Socket.IO/socket.io.js'
+			, '/public/js/client.js'
+			, '/public/external/json2.js'
 		]
 	}, 'all_css': {
 		'route': /\/css\/all_[0-9]+\.css/
-		, 'path': './public/css/'
+		, 'path': pub + '/css'
 		, 'dataType': 'css'
 		, 'files': [
-			'base.css'
-			, 'landing.css'
-			, 'twitter.css'
+			'/base.css'
+			, '/landing.css'
+			, '/twitter.css'
 		]
 		, 'preManipulate': {
 			// Regexp to match user-agents including MSIE.
@@ -42,7 +47,7 @@ var assetManagerGroups = {
 				assetHandler.yuiCssOptimize
 				, assetHandler.fixVendorPrefixes
 				, assetHandler.fixGradients
-				, assetHandler.replaceImageRefToBase64('public')
+				, assetHandler.replaceImageRefToBase64(pub)
 			]
 		}
 	}
