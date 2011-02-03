888dc6a6a3caa33162ba3a780aa2b7efe735511c
Daniel Beardsley <daniel.beardsley@gmail.com>
2011-02-03 03:02:04 -0800

    CSS is now grouped, minified, and images are base-64 encoded

--- nodejs/lib/asset_manager.js
+++ nodejs/lib/asset_manager.js
@@ -2,51 +2,51 @@ var assetManager = require('connect-assetmanager');
 var assetHandler = require('connect-assetmanager-handlers');
 
 var assetManagerGroups = {
-    'landing_js': {
-        'route': /\/js\/landing_[\d]+.js/
-        , 'path': './public/'
-        , 'dataType': 'javascript'
-        , 'files': [
-            '/external/events.js'
-        ]
-    },		
-    'channel_js': {
-        'route': /\/js\/channel_[\d]+.js/
-        , 'path': './'
-        , 'dataType': 'javascript'
-        , 'files': [
-            'public/external/events.js'
-						, 'vendor/Socket.IO/socket.io.js'
-						, 'public/js/client.js'
-						, 'public/external/json2.js'
-        ]
-    }		/*, 'css': {
-        'route': /\/static\/css\/[0-9]+\/.*\.css/
-        , 'path': './public/css/'
-        , 'dataType': 'css'
-        , 'files': [
-            'reset.css'
-            , 'style.css'
-        ]
-        , 'preManipulate': {
-            // Regexp to match user-agents including MSIE.
-            'MSIE': [
-                assetHandler.yuiCssOptimize
-                , assetHandler.fixVendorPrefixes
-                , assetHandler.fixGradients
-                , assetHandler.stripDataUrlsPrefix
-            ],
-            // Matches all (regex start line)
-            '^': [
-                assetHandler.yuiCssOptimize
-                , assetHandler.fixVendorPrefixes
-                , assetHandler.fixGradients
-                , assetHandler.replaceImageRefToBase64(root)
-            ]
-        }
-    }*/
+	'landing_js': {
+		'route': /\/js\/landing_[\d]+.js/
+		, 'path': './public/'
+		, 'dataType': 'javascript'
+		, 'files': [
+			'/external/events.js'
+		]
+	},		
+	'channel_js': {
+		'route': /\/js\/channel_[\d]+.js/
+		, 'path': './'
+		, 'dataType': 'javascript'
+		, 'files': [
+			'public/external/events.js'
+			, 'vendor/Socket.IO/socket.io.js'
+			, 'public/js/client.js'
+			, 'public/external/json2.js'
+		]
+	}, 'all_css': {
+		'route': /\/css\/all_[0-9]+\.css/
+		, 'path': './public/css/'
+		, 'dataType': 'css'
+		, 'files': [
+			'base.css'
+			, 'landing.css'
+		]
+		, 'preManipulate': {
+			// Regexp to match user-agents including MSIE.
+			'MSIE': [
+				assetHandler.yuiCssOptimize
+				, assetHandler.fixVendorPrefixes
+				, assetHandler.fixGradients
+				, assetHandler.stripDataUrlsPrefix
+			],
+			// Matches all (regex start line)
+			'^': [
+				assetHandler.yuiCssOptimize
+				, assetHandler.fixVendorPrefixes
+				, assetHandler.fixGradients
+				, assetHandler.replaceImageRefToBase64('public')
+			]
+		}
+	}
 };
 
 module.exports = {
-		middleware: assetManager(assetManagerGroups)
+	middleware: assetManager(assetManagerGroups)
 }
