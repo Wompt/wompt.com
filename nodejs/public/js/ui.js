9c4078a96be2cd217480db64bed0c0fdb1a4fdc1
Daniel Beardsley <daniel.beardsley@gmail.com>
2011-02-28 18:08:43 -0800

    Remove remnants of JS layout resizing code

--- nodejs/public/js/ui.js
+++ nodejs/public/js/ui.js
@@ -4,24 +4,6 @@ function UI(){};
 UI.prototype = new EventEmitter();
 UI = new UI();
 
-// fires a custom event after the user has finished resizing the window
-UI.once('init', function(){
-	var UI = this
-	  , settle_time = 100 // ms to wait before firing our event
-	  , resize_timer;
-	
-	function emitResized(){
-		UI.emit('resized');
-		resize_timer = null;
-	}
-	
-	$(window).resize(function(){
-		if(resize_timer) clearTimeout(resize_timer);
-		resize_timer = setTimeout(emitResized, settle_time);
-	});	
-});
-
-
 UI.once('init', function(){	
 	if(!readonly){
 		IO.socket.on('connect', function(){
