b27bad69c822f49f8a7d1d063491a20f1e506171
Daniel Beardsley <daniel.beardsley@gmail.com>
2011-03-14 17:31:03 -0700

    Bug Fix in nodejs enviroment code

--- nodejs/environment.js
+++ nodejs/environment.js
@@ -3,7 +3,7 @@ var default_env = require("./environment/defaults");
 var environment = process.env.NODE_ENV || 'development';
 var specified_env = require("./environment/" + environment);
 
-require('./lib/util').mergeDeep(specified_env, default_env);
+specified_env = require('./lib/util').mergeCopy(default_env, specified_env);
 
 console.log("Using environment: " + environment);
 specified_env.name = environment;
