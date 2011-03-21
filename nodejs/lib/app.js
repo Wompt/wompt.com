137643bc353436b3bc4d00a8eeaaa3bddfcd2844
Daniel Beardsley <daniel.beardsley@gmail.com>
2011-03-21 14:12:25 -0700

    Order middleware so favicon actually gets served by the favicon middleware.

--- nodejs/lib/app.js
+++ nodejs/lib/app.js
@@ -42,9 +42,8 @@ App.prototype = {
 			exp.use(wompt.Auth.forward_to_auth_app_middleware());
 			exp.use(express.logger({format: ':method :url :status :response-time' }));
 			exp.use(assetManager.middleware);
-			exp.use(express.static(config.public_dir));
-			// Need to serve up the apple itouch icon too
 			exp.use(express.favicon(config.public_dir + '/favicon.ico'));
+			exp.use(express.static(config.public_dir));
 			exp.use(express.cookieParser());
 			exp.use(express.bodyParser());
 			exp.use(wompt.Auth.one_time_token_middleware());
