4b5d2af8cfffc1c3aad84ad56cc3d330df74b7d2
Daniel Beardsley <daniel.beardsley@gmail.com>
2011-03-21 14:09:36 -0700

    Load Commit id (supplied by capistrano) into the wompt environment config

--- nodejs/environment.js
+++ nodejs/environment.js
@@ -1,11 +1,21 @@
-var default_env = require("./environment/defaults");
+var default_env = require("./environment/defaults"),
+util = require('./lib/util');
 
 var environment = process.env.NODE_ENV || 'development';
 var specified_env = require("./environment/" + environment);
 
-specified_env = require('./lib/util').mergeCopy(default_env, specified_env);
+specified_env = util.mergeCopy(default_env, specified_env);
 
 console.log("Using environment: " + environment);
+
 specified_env.name = environment;
+
+// REVISION is a file containing the deployed commit ID created by Capistrno
+if(util.fs.readFile(specified_env.deploy_root + '/REVISION', function(text, exists){
+	var version = text || Math.random().toString();
+	var hash = util.md5(version);
+	util.mergeDeep(specified_env, {constants: {version_hash: hash}});
+}));
+
 
 module.exports = specified_env;
--- nodejs/environment/defaults.js
+++ nodejs/environment/defaults.js
@@ -1,13 +1,15 @@
 var path = require('path');
 
 // strip the last dir...  essentially the same as __dirname/..
-var root = path.dirname(__dirname); //.replace(/\/[^\/]+\/?$/,'');
+var root = path.dirname(__dirname); //.replace(/\/[^\/]+\/?$/,''),
+deploy_root = path.normalize(root + '/..');
 
 module.exports = {
 	  port: 8001
 	, public_dir: root + '/public'
 	, db_name: 'wompt_dev'
 	, root: root
+	, deploy_root: deploy_root
 	, app_host:  '127.0.0.1'
 	, db_host:   '127.0.0.1'
 	, auth_host: '127.0.0.1'
