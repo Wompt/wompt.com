9b05dbc81843aeb0dcf49906e10042ef9494e69b
Daniel Beardsley <daniel.beardsley@gmail.com>
2011-03-23 01:00:29 -0700

    refactor UI MessageList.newMessage function for clarity

--- nodejs/public/js/message_list.js
+++ nodejs/public/js/message_list.js
@@ -7,16 +7,15 @@ function MessageList(){
 		if(msg.action == 'message'){
 			this.list.push(msg);
 			this.emit('appended', msg);
-			return true;
 		}else if(msg.action == 'previous'){
 			var me = this;
 			$.each(msg.messages, function(i, m){
 				me.list.push(m);
 			});
 			me.emit('batch_appended', msg.messages);
-			return true;
-		}
-		return false;
+		}else
+			return false;
+		return true;
 	}
 }
 
