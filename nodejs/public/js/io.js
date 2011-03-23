744616f1402c06caa2a61b7e042d2dc0f3e8f7b0
Daniel Beardsley <daniel.beardsley@gmail.com>
2011-03-23 16:58:55 -0700

    Refactor client IO.js

--- nodejs/public/js/io.js
+++ nodejs/public/js/io.js
@@ -8,19 +8,21 @@ function IO(){
 
 	var messageHandlers = [];
 
-	socket.on('message', function(data){
+	this.processMessage = function(data){
 		for(var i=0, len=messageHandlers.length; i<len; i++){
 			var handler = messageHandlers[i];
 			if(handler.newMessage(data)) break;
 		}
-	});
+	}
+	
+	this.addMessageHandler = function(handler){
+		messageHandlers.push(handler);
+	}
 	
 	this.connect = function(){
 		socket.connect();
 		socket.send({channel: channel, action: 'join', connector_id: connector_id});
 	}
 	
-	this.addMessageHandler = function(handler){
-		messageHandlers.push(handler);
-	}
+	socket.on('message', this.processMessage);
 }
