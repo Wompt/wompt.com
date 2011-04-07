0c88e02036d5bde21f2345c8df49877c78805008
Daniel Beardsley <daniel.beardsley@gmail.com>
2011-04-07 01:42:04 -0700

    Since we don't need message list anymore, keep fewer messages

--- nodejs/public/js/message_list.js
+++ nodejs/public/js/message_list.js
@@ -1,5 +1,6 @@
 function MessageList(){
 	this.list = [];
+	var messages_to_keep = 1;
 	
 	// message handler for our IO class
 	this.newMessage = function(msg){
@@ -11,6 +12,7 @@ function MessageList(){
 				me.list.push(m);
 			});
 		}
+		this.trim();
 	}
 	
 	this.last = function(){
@@ -20,5 +22,12 @@ function MessageList(){
 	this.lastTimeStamp = function(){
 		var l = this.last();
 		return l && l.t;
+	}
+	
+	this.trim = function(){
+		var num_to_remove = this.list.length - messages_to_keep;
+		if(num_to_remove <=0) return;
+		
+		this.list.splice(0,num_to_remove);
 	}
 }
