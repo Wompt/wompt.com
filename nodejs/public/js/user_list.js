868639d8617633cb71594e5c60dc9d914cf47814
Daniel Beardsley <daniel.beardsley@gmail.com>
2011-04-12 23:42:24 -0700

    Keep user list UI sorted by name

--- nodejs/public/js/user_list.js
+++ nodejs/public/js/user_list.js
@@ -35,7 +35,8 @@ UserList.prototype.each = function(f){
 
 function UserListUI(ul, container){
 	if(!ul) return;
-	var user_divs = {};
+	var user_divs = {},
+	sorted = [];
 	
 	ul.on('join', function(users){
 		var names=[];
@@ -64,19 +65,37 @@ function UserListUI(ul, container){
 	});
 	
 	function addUser(user){
-		var name_div = $('<div>');
+		if(user_divs[user.id]) return;
+		
+		var name_div = user.el = $('<div>');
 		name_div.attr('id', 'user_' + user.id);
 		name_div.attr('title', user.name);
 		name_div.css('color', UI.Colors.forUser(user.id))
 		name_div.append(user.name);
 		name_div.addClass('user');
-		container.append(name_div);
+		var insert_after = addToSortedList(user);
+		if(insert_after)
+			insert_after.el.after(name_div);
+		else
+			container.prepend(name_div);
 		user_divs[user.id] = name_div;
+	}
+	
+	function addToSortedList(user){
+		sorted.push(user);
+		sorted.sort(function(a,b){
+			return a.name.localeCompare(b.name);
+		});
+		var i=sorted.indexOf(user);
+		return i > 0 ? sorted[i-1] : null;
 	}
 	
 	function removeUser(user){
 		if(user_divs[user.id]){
 			user_divs[user.id].remove();
+			sorted = sorted.filter(function(each){
+				return user.id != each.id;
+			});
 			delete user_divs[user.id];
 		}
 	}
@@ -86,5 +105,6 @@ function UserListUI(ul, container){
 			user_divs[i].remove();
 		}
 		user_divs = {};
+		sorted = [];
 	}
 }
