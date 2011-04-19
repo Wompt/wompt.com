edaf035a3654f5297f49f1e0857f8fcdb651a95c
Daniel Beardsley <daniel.beardsley@gmail.com>
2011-04-19 00:16:04 -0700

    Add function to close lightbox and send Socket.IO message

--- nodejs/public/js/ui.popup.js
+++ nodejs/public/js/ui.popup.js
@@ -4,6 +4,13 @@ UI.once('init', function(){
 			if(msg.action == 'popup'){
 				UI.lightbox.show(msg.options);
 			}
+		},
+		respondWith: function(msg){
+			if(IO.socket){
+				msg.action='popup/response';
+				IO.socket.send(msg);
+			}
+			UI.lightbox.close();
 		}
 	};
 });
