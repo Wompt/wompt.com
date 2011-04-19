cc536ea3be5bb506b918b630841d0e875fac1502
Daniel Beardsley <daniel.beardsley@gmail.com>
2011-04-19 13:34:33 -0700

    Make enter key submit Name Requester dialog

--- nodejs/public/js/popup/name_requester.js
+++ nodejs/public/js/popup/name_requester.js
@@ -1,11 +1,17 @@
 (function(){
 	var id = 'name_requestor',
 	popup = $('#' + id);
-	
-	popup.find('#done').click(function(){
+	popup.find('#done').click(submit);
+	popup.find('#new_name').keydown(function(e){
+		if(e.which == 13){
+			submit();
+			e.preventDefault();
+		}
+	});
+	function submit(){
 		UI.Popup.respondWith({
 			context:id,
 			name:popup.find('#new_name').val()
 		});
-	});
+	}	
 })();
