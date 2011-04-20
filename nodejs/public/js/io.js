093eaaf163b11995fcfa391c4af42d682b3838f4
Daniel Beardsley <daniel.beardsley@gmail.com>
2011-04-20 11:52:08 -0700

    Remove "port" setting for Socket.IO config It was messing up the websocket url in Firefox

--- nodejs/public/js/io.js
+++ nodejs/public/js/io.js
@@ -2,8 +2,7 @@ function IO(){
 	var secure = window.location.protocol.match(/https/);
 	var socket = this.socket = new io.Socket(window.location.hostname, {
 			secure: secure,
-			port: secure ? '' : window.location.port
-			,reconnectionDelay: 2000 + Math.random() * 2000
+			reconnectionDelay: 2000 + Math.random() * 2000
 		});
 
 	var messageHandlers = [];
