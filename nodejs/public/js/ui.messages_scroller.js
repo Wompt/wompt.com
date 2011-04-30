41e869d47c75048d86874c560cd9b0612818b52c
Daniel Beardsley <daniel.beardsley@gmail.com>
2011-04-30 16:54:57 -0700

    Fixed scrolling bug, and made content scroll into view even when the scroll-bar isn't visible

--- nodejs/public/js/ui.messages_scroller.js
+++ nodejs/public/js/ui.messages_scroller.js
@@ -1,34 +1,49 @@
 UI.once('init', function(){
 	var UI = this
 	  , should_scroll = true
-		, msgDiv = $("#messages_scroller")
+		, pane = $("#messages_scroller")
+		, paneEl = pane.get(0)
 		, scrolling = false
 		, animateScroll = true
+		, bottom_dist
+		, old_height
 		// When the height of the content scrolled beneath the viewport is greater than this
 		// don't auto-scroll
-		, SCROLL_LIMIT = 30;
+		, SCROLL_LIMIT = 40;
 
 	var me = UI.messagesScroller = {				
 		scrollToBottom: function(){
+			var destScroll = paneEl.scrollHeight - pane.innerHeight(); // height of content - height of viewport (always <= height of content)
+			
 			if(animateScroll){
-				var destScroll = (msgDiv.get(0).scrollHeight - msgDiv.innerHeight()),
-				    distance = destScroll - msgDiv.scrollTop();
+				var distance = destScroll - pane.scrollTop(), // distance to scroll the viewport
+				added_height = paneEl.scrollHeight - old_height, // height of new content
+				end_at = {scrollTop: destScroll, bottom:0};
+
+				if(destScroll == 0){ // the content is smaller than the viewport
+					pane.css('bottom',-added_height); //we have to slide the content from slightly below the viewport, instead of scrolling it
+					distance = added_height;
+				}
 				
-				scrolling = true;
-				msgDiv.stop().animate(
-					{scrollTop: destScroll},
-					200 + Math.sqrt(distance) * 10,
-					'swing',
-					function(){scrolling = false;}
-				);
+				if(distance > 0){
+					scrolling = true;
+					pane.stop().animate(
+						end_at,
+						200 + Math.sqrt(distance) * 10,
+						'swing',
+						function(){scrolling = false;}
+					);
+				}
 			}else{
-				msgDiv.scrollTop(msgDiv.get(0).scrollHeight);
+				pane.scrollTop(destScroll);
 			}
 		},
 		
 		checkShouldSroll: function(data){
-			// content - hidden area above viewport - viewport = area scrolled below viewport
-			should_scroll = scrolling || (msgDiv.get(0).scrollHeight - msgDiv.scrollTop() - msgDiv.innerHeight()) < SCROLL_LIMIT;
+			// content height - hidden area above viewport - viewport = area scrolled below viewport
+			old_height = paneEl.scrollHeight;
+			bottom_dist = old_height - pane.scrollTop() - pane.innerHeight();
+			should_scroll = scrolling || (bottom_dist < SCROLL_LIMIT);
 		}
 	}
 	
