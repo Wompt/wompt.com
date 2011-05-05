1fbf0470747fbab785f64c9ac9ce0d7d3df9f30d
Daniel Beardsley <daniel.beardsley@gmail.com>
2011-05-05 15:39:50 -0700

    TEMPORARY COMMIT

--- nodejs/public/js/landing.js
+++ nodejs/public/js/landing.js
@@ -1,3 +1,26 @@
 $(function(){
 	UI.emit('init');
+
+	var slides = $('.slides .slide').get(), current = 1;
+	function showNext(){
+		var i=0;
+		$.each(slides, function(key, slide){
+			var s = $(slide);
+			
+			s[addClassIf(-1)]('past')
+			[addClassIf(0)]('current')
+			[addClassIf(1)]('future');
+			i++;
+		});
+		current = (current + 1) % i;
+		console.log(current);
+		function addClassIf(inc){
+			var x = (current + inc) % slides.length;
+			if(x < 0) x = x + slides.length;
+			return (x == i ? 'add' : 'remove') + 'Class';
+		}
+	}
+	setInterval(showNext, 5000);
 });
+
+
