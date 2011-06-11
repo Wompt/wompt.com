25a923db7307e38e1cabe3e86056b24590ff27df
Daniel Beardsley <daniel.beardsley@gmail.com>
2011-06-11 16:08:24 -0700

    Admin-only accounts index page

--- nodejs/lib/controllers/accounts.js
+++ nodejs/lib/controllers/accounts.js
@@ -1,5 +1,24 @@
-module.exports = function AccountsController(wompt, app){
-	this.index = function(req,res){
-		res.send("Hello world!");
+var wompt = require("../includes");
+
+function AccountsController(app){
+	this.index = function(req, res, next){
+		if(blockNonAdmins(req, res, next)) return;
+		res.end('Hello Admin');
+	}
+	
+	this.load = function loadAccount(name, fn){
+		app.accountManager.get(name, fn);
 	}
 }
+
+
+function blockNonAdmins(req, res, next){
+	if(req.user && req.user.is_admin()){
+		return false;
+	}else{
+		next(new wompt.errors.NotFound());
+		return true;
+	}
+}
+
+module.exports = AccountsController
