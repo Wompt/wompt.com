fa84ece1e0d07182dbf781b8c8e50ac3504e9707
Daniel Beardsley <daniel.beardsley@gmail.com>
2011-06-11 17:10:40 -0700

    Remove unused if statement in environment.js

--- nodejs/environment.js
+++ nodejs/environment.js
@@ -17,12 +17,12 @@ console.log("Using environment: " + environment);
 specified_env.name = environment;
 
 // REVISION is a file containing the deployed commit ID created by Capistrno
-if(util.fs.readFile(specified_env.deploy_root + '/REVISION', function(text, exists){
+util.fs.readFile(specified_env.deploy_root + '/REVISION', function(text, exists){
 	var version = text || Math.random().toString();
 	var hash = util.md5(version);
 	util.mergeDeep(specified_env, {constants: {version_hash: hash}});
 	precomputeConstantsJSON();
-}));
+});
 
 
 module.exports = specified_env;
