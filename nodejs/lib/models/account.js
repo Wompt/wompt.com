6b97582e2667a833b0bd05e006687b04320c80d9
Daniel Beardsley <daniel.beardsley@gmail.com>
2011-06-11 20:16:04 -0700

    Add owner_ids field to Accounts

--- nodejs/lib/models/account.js
+++ nodejs/lib/models/account.js
@@ -1,9 +1,11 @@
 var wompt = require("../includes"),
-    mongoose = wompt.mongoose;
+mongoose = wompt.mongoose,
+ObjectId = mongoose.Schema.ObjectId;
 
 var Account = new mongoose.Schema({
 	'display_name'    : String
 	,'name'           : String
+	,'owner_ids'      : [ObjectId]
 });
 
 // number defines index sort order (1=asc)
