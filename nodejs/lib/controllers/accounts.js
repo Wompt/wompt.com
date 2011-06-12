9af364652f2c2df927659b792f3d7869988cea02
Daniel Beardsley <daniel.beardsley@gmail.com>
2011-06-11 20:09:44 -0700

    Add some comments to AccountsController

--- nodejs/lib/controllers/accounts.js
+++ nodejs/lib/controllers/accounts.js
@@ -1,6 +1,7 @@
 var wompt = require("../includes");
 
 function AccountsController(app){
+	// url: /accounts
 	this.index = blockNonAdmins(function index(req, res, next){
 		wompt.Account.find(function(err, results){
 			res.render('accounts/index', app.standard_page_vars(req, {
@@ -9,16 +10,19 @@ function AccountsController(app){
 		});
 	})
 	
+	// url: /accounts/:id
 	this.show = blockNonAdmins(function show(req, res, next){
 		res.render('accounts/show', app.standard_page_vars(req, {
 			account: req.account
 		}));
 	})
 	
+	// url: /accounts/new
 	this.new = blockNonAdmins(function _new(req, res, next){
 		res.render('accounts/new', app.standard_page_vars(req));
 	})
 	
+	// url: POST /accounts
 	this.create = blockNonAdmins(function create(req, res, next){
 		var account = new wompt.Account();
 		['name'].forEach(function(key){
@@ -32,6 +36,7 @@ function AccountsController(app){
 		})
 	})
 	
+	// called for each of the above actions that use an :id sets req.account
 	this.load = function loadAccount(name, fn){
 		app.accountManager.get(name, fn);
 	}
