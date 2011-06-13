7f6028d6db4909a609314c91580e664195d29156
Daniel Beardsley <daniel.beardsley@gmail.com>
2011-06-13 15:18:42 -0700

    Add Account.secret field to mongodb

--- nodejs/lib/models/account.js
+++ nodejs/lib/models/account.js
@@ -6,11 +6,21 @@ var Account = new mongoose.Schema({
 	'display_name'    : String
 	,'name'           : String
 	,'owner_ids'      : [ObjectId]
+	,'secret'         : {type: String, 'default': generateSecret}
 });
 
 // number defines index sort order (1=asc)
 Account.index({name: 1});
 
+Account.static({
+	generateSecret: generateSecret
+});
+
 // Model name, Schema, collection name
 mongoose.model('Account', Account, 'accounts');
 module.exports = mongoose.model('Account');
+
+
+function generateSecret(){
+	return wompt.util.randomString(20);
+}
