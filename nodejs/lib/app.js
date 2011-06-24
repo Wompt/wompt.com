0597b82b92a7e68677aed04dad1360705825c7cd
Daniel Beardsley <daniel.beardsley@gmail.com>
2011-06-24 12:14:46 -0700

    Remove duplicate exemption from static server root file access

--- nodejs/lib/app.js
+++ nodejs/lib/app.js
@@ -94,7 +94,6 @@ App.prototype = {
 				,'/google8a88ebb03a6df8aa.html': true
 				,'/robots.txt':true
 				,'/google5099d06e90a418ce.html': true
-				,'/fb_cross_domain.html': true
 			}
 			exp.use(function checkStaticAndStrip(req,res,next){
 				var url = req.url;
