abad712d1e40d83504e91751d2b9b47e07807d5e
Daniel Beardsley <daniel.beardsley@gmail.com>
2011-07-06 00:35:57 -0700

    Add first basic test of Util.merge

--- nodejs/test/util.js
+++ nodejs/test/util.js
@@ -1,14 +1,32 @@
 var Util = require('../lib/util'),
 node_util = require('util'),
+assert = require('assert'),
 vows = require('wompt_dependencies').vows;
 
 var suite = vows.describe('Util module');
 
 suite.addBatch({
-	'Util.merge':{
-		topic: function(){ return blah; },
-		'blah': function(){
+	'Util.merge for single-level merges':{
+		topic: function(){
+			return {
+				a: 1,
+				b: "two",
+				c: {three:true}
+			}
+		},
+		
+		'with an empty target': function(original){
+			var empty = {};
+			var merged = Util.merge(empty, original);
 			
+			// Equal keys/values
+			assert.deepEqual(original, merged);
+			
+			// Not the same object
+			assert.notEqual(original, merged);
+			
+			// Same object
+			assert.strictEqual(empty, merged);
 		}
 	}
-}).run();
+}).export(module);
