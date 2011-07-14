d2ffe7187088229926c3e0a8b9205a5318a59310
Daniel Beardsley <daniel.beardsley@gmail.com>
2011-07-13 23:46:57 -0700

    Add Boiler Plate Test

---
+++ nodejs/test/test_boiler.js
@@ -0,0 +1,9 @@
+// Boiler Plate test file -- copy and modify.
+var W = require('./test_helper'),
+assert = W.assert,
+vows = W.vows;
+
+vows.describe('Thing being tested')
+.addBatch({
+	
+}).export(module);
