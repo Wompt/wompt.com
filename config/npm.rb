52460f486debe143851015295533e0eabcdae6d9
Daniel Beardsley <daniel.beardsley@gmail.com>
2011-12-04 23:52:12 -0800

    Changes for npm 1.0, move package.json to root
    Add node_modules symlinks on deploy

--- config/npm.rb
+++ config/npm.rb
@@ -1,5 +1,7 @@
 namespace :deploy do
 	namespace :npm do
+		after "deploy:update_code", "deploy:npm:create_symlinks"
+
 		desc "Deploy and install missing npm dependencies on the server"
 		task :default, :roles => :app do
 			after 'deploy:update', 'deploy:npm:install'
@@ -8,7 +10,13 @@ namespace :deploy do
 
 		desc "Install missing npm dependencies on the server"
 		task :install, :roles => :app do
-			run "npm install #{current_path}/nodejs/deps"
+			run "mkdir -p #{shared_path}/node_modules"
+			run "cd #{current_path}/nodejs && npm install "
+		end
+		
+		desc "Create the necessary symlinks for npm"
+		task :create_symlinks do
+			run "ln -nfs #{shared_path}/node_modules #{release_path}/nodejs/node_modules"
 		end
 	end
 end
