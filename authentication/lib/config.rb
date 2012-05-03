a048ca0e23187a17d10585302c509ae7ccc7188f
Daniel Beardsley <daniel@ifixit.com>
2012-05-03 00:37:26 -0700

    Use new ruby 1.9 require syntax

--- authentication/lib/config.rb
+++ authentication/lib/config.rb
@@ -1,5 +1,5 @@
-require "config/defaults"
-require "config/#{settings.environment}"
+require "./config/defaults"
+require "./config/#{settings.environment}"
 
 puts "#Starting in #{settings.environment} mode with config:"
 puts "##{CONFIG.inspect}"
